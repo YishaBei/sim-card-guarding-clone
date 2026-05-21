2bc950d88bd7014cc6739c846454a5ba776b075c
Misaki <176287697+YishaBei@users.noreply.github.com>
2026-05-21 16:54:12 +0800

    Add shared state and API helper

---
+++ state.js
@@ -0,0 +1,2 @@
+window.G={s:{u:null,c:null,g:null,m:[{t:'已成功添加被守护人',d:'您已成功添加被守护人，点击查看守护详情。',b:'通知',time:'今天 09:18'},{t:'请及时完成实名认证',d:'为使用守护区域与位置查询，请先完成实名认证。',b:'提醒',time:'昨天'},{t:'恭喜您获得流量福利',d:'现在可点击领取，数量有限，领完即止。',b:'福利',time:'周三'}],r:[{n:'家',d:'广东省深圳市南山区 科技园附近',on:true},{n:'学校',d:'广东省深圳市南山区 学校周边',on:false}],p:{'/locationPolicy':'智能提醒及位置隐私政策','/collectPolicy':'个人信息使用与收集清单','/guardianPolicy':'用户协议与守护宝说明','/privacyPolicy':'隐私政策','/privacyPolicyZy':'隐私政策摘要','/childrenPolicy':'守护宝儿童隐私保护声明','/xiheFamily':'羲和防诈家庭版说明'}}};
+window.api=p=>fetch(p).then(r=>r.json()).catch(()=>null);
